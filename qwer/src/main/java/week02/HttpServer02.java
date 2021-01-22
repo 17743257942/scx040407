2794ceedbf41febb51d3b995fe01e56673714a81
17743257942 <546294903@qq.com>
2021-01-22 14:27:10 +0800

    2021-01-22 14:26

---
+++ qwer/src/main/java/week02/HttpServer02.java
@@ -0,0 +1,34 @@
+package week02;
+
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer02 {
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(8802);
+        while (true) {
+            Socket socket = serverSocket.accept();
+            new Thread(() -> service(socket)).start();
+        }
+    }
+
+    private static void service(Socket socket) {
+        try {
+//            Thread.sleep(500);
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            printWriter.println("HTTP/1.1 200 OK");
+            printWriter.println("Content-Type:text/html;charset=utf-8");
+            String body = "hello,nio2";
+            printWriter.println("Content-Length:" + body.getBytes().length);
+            printWriter.println();
+            printWriter.write(body);
+            printWriter.close();
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ qwer/src/main/java/week02/HttpServer03.java
@@ -0,0 +1,43 @@
+package week02;
+
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class HttpServer03 {
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(8803);
+        ExecutorService executorService = Executors.newFixedThreadPool(
+                Runtime.getRuntime().availableProcessors() + 2);
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    service(socket);
+                }
+            });
+        }
+    }
+
+    private static void service(Socket socket) {
+        try {
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            printWriter.println("HTTP/1.1 200 OK");
+            printWriter.println("Content-Type:text/html;charset=utf-8");
+            String body = "hello,nio3";
+            printWriter.println("Content-Length:" + body.getBytes().length);
+            printWriter.println();
+            printWriter.write(body);
+            printWriter.close();
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
