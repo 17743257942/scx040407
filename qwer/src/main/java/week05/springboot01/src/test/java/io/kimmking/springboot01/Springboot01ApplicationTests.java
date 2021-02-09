fe830baa93a99ec8fd793643695778539b867485
17743257942 <546294903@qq.com>
2021-02-09 16:45:24 +0800

    aa

---
+++ qwer/src/main/java/week05/springboot01/src/test/java/io/kimmking/springboot01/Springboot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package week05.springboot01.src.test.java.io.kimmking.springboot01;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
