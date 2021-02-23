71aa450f8844b1b80bff1c55eee4d87ea79c040c
17743257942 <546294903@qq.com>
2021-02-23 12:17:38 +0800

    啊

---
+++ qwer/src/main/java/week04/_1/Test.java
@@ -0,0 +1,22 @@
+package week04._1;
+
+public class Test {
+    public static void main(String[] args) {
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Thread t = Thread.currentThread();
+                System.out.println("current thread:" + t.getName());
+            }
+        };
+        Thread thread = new Thread(task);
+        thread.setName("test-thread-1");
+//        thread.setDaemon(true);
+        thread.start();
+    }
+}
