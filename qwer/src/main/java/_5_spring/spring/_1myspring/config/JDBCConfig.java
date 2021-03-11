507e15b3ac414e1956f469a897fc6fc5132b1fc7
17743257942 <546294903@qq.com>
2021-03-11 22:12:47 +0800

    aop

--- qwer/src/main/java/_5_spring/spring/_1myspring/config/JDBCConfig.java
+++ qwer/src/main/java/_5_spring/spring/_1myspring/config/JDBCConfig.java
@@ -2,6 +2,7 @@ package _5_spring.spring._1myspring.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.apache.commons.dbutils.QueryRunner;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
@@ -17,6 +18,9 @@ public class JDBCConfig {
     @Value("${jdbc.url}")
     private String url;
 
+    @Value("${jdbc.url2}")
+    private String url2;
+
     @Value("${jdbc.username}")
     private String username;
 
@@ -25,15 +29,25 @@ public class JDBCConfig {
 
     @Bean("runner")
     @Scope("prototype")
-    public QueryRunner createQueryRunner(DataSource dataSource) {
+    public QueryRunner createQueryRunner(@Qualifier("ds1") DataSource dataSource) {
         return new QueryRunner(dataSource);
     }
 
-    @Bean(name = "dataSource")
+    @Bean(name = "ds1")
     public DataSource createDataSource() throws PropertyVetoException {
         ComboPooledDataSource ds = new ComboPooledDataSource();
         ds.setDriverClass(driver);
         ds.setJdbcUrl(url);
+        ds.setUser(username);
+        ds.setPassword(password);
+        return ds;
+    }
+
+    @Bean(name = "ds2")
+    public DataSource createDataSource1() throws PropertyVetoException {
+        ComboPooledDataSource ds = new ComboPooledDataSource();
+        ds.setDriverClass(driver);
+        ds.setJdbcUrl(url2);
         ds.setUser(username);
         ds.setPassword(password);
         return ds;
