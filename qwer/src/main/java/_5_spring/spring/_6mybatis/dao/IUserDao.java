d0b6d0f1264dffb912e0947cb5633c231d4917b7
17743257942 <546294903@qq.com>
2021-03-20 21:51:13 +0800

    mybatis

--- qwer/src/main/java/_5_spring/spring/_6mybatis/dao/IUserDao.java
+++ qwer/src/main/java/_5_spring/spring/_6mybatis/dao/IUserDao.java
@@ -1,9 +1,11 @@
 package _5_spring.spring._6mybatis.dao;
 
 import _5_spring.spring._6mybatis.domain.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface IUserDao {
+    @Select("select * from user")
     List<User> findAll();
 }
